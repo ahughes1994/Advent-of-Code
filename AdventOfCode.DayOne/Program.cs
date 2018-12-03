9beb338e7ab9d0f0e5081c9732640d3f405469cd
Anthony Hughes <anthony@anthonyhughes.work>
2018-12-03 18:47:56 +0000

    Complete day 1, part2

--- AdventOfCode.DayOne/Program.cs
+++ AdventOfCode.DayOne/Program.cs
@@ -1,11 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AdventOfCode.DayOne
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Part2Main(string[] args)
+        {
+            var usedFreqs = new List<int>();
+            var total = 0;
+            int? repeat = null;
+            usedFreqs.Add(total);
+
+            while (repeat == null)
+            {
+                using (var input = new StreamReader(@"input2.txt"))
+                {
+                    string line;
+                    while ((line = input.ReadLine()) != null)
+                    {
+                        if (int.TryParse(line, out var result))
+                        {
+                            total += result;
+                            if (usedFreqs.Contains(total))
+                            {
+                                repeat = total;
+                                break;
+                            }
+                            else
+                            {
+                                usedFreqs.Add(total);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(total.ToString());
+            Console.ReadLine();
+        }
+
+        static void Part1Main(string[] args)
         {
             var total = 0;
             using (var input = new StreamReader(@"input.txt"))
